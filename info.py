f177435592e90792ecac493abb133eb581fff9e6
THEHYPER-ROBOT <THEHYPER1232@gmail.com>
2025-04-01 23:09:56 +0530

    Update info.py

--- info.py
+++ info.py
@@ -39,7 +39,7 @@ auth_grp = environ.get('AUTH_GROUP')
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 reqst_channel = environ.get('REQST_CHANNEL_ID', '-1002326594935') 
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
-support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1002401464716') 
+support_chat_id = environ.get('SUPPORT_CHAT_ID', '-4661632171') 
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
 
 DATABASE_URI = environ.get('DATABASE_URI', "")
